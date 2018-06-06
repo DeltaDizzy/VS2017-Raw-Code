3dca812e5bcf339f7da5a0b23640273b565988f0
DeltaDizzy <EJBraswell@gmail.com>
2018-06-06 11:17:24 -0400

    an experiment...

---
+++ ConfigNode/ConfigParser.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace ConfigParser
+{
+    class Parser : MonoBehaviour
+    {
+        public ConfigNode Parse(ConfigNode node)
+        {
+            ConfigNode parseNode = node;
+            return parseNode;
+        }
+    }
+}
